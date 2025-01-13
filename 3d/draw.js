efb2d27980aedc35e783ef66ce3d4a1080822e40
Jake <jake@vuca.ai>
2025-01-12 20:39:33 -0800

    .

--- 3d/draw.js
+++ 3d/draw.js
@@ -9,26 +9,4 @@ function draw() {
     fill(0, 0, 255);
     box(32);
     
-    // Blue cylinder on +z axis
-    push();
-    translate(0, 0, 132);
-    rotateX(PI/2);
-    fill(0, 0, 255);
-    cylinder(10, 64);
-    pop();
-    
-    // Red cylinder on +x axis
-    push();
-    translate(132, 0, 0);
-    rotateZ(-PI/2);
-    fill(255, 0, 0);
-    cylinder(10, 64);
-    pop();
-    
-    // Green cylinder on +y axis
-    push();
-    translate(0, 132, 0);
-    fill(0, 255, 0);
-    cylinder(10, 64);
-    pop();
 }
