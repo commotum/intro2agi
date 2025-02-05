06fd8b1cba0bfb25704cee7e7522adae9b393c76
Jake <jake@vuca.ai>
2025-02-05 01:32:16 -0800

    .

--- ECE-401/03 - 3d/components/tasks/types.ts
+++
@@ -1,9 +0,0 @@
-import p5 from 'p5'
-
-export interface P5Task {
-  /** Width of the canvas in pixels */
-  width: number
-  /** Height of the canvas in pixels */ 
-  height: number
-  draw: (p: p5) => void
-}
